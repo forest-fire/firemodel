b007779df3c934db058dc7517d21ed3ccba3c432
Ken Snyder <ken@ken.net>
2018-06-29 09:38:38 -0700

    added some exports that were missing

--- dist/esnext/index.js
+++ dist/esnext/index.js
@@ -5,5 +5,7 @@ export { Model, RelationshipPolicy, RelationshipCardinality } from "./Model";
 export { Record } from "./Record";
 export { List } from "./List";
 export { Mock } from "./Mock";
+export { FireModel } from "./FireModel";
+export { Audit } from "./Audit";
 export { key as fbKey } from "firebase-key";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,8 @@ export { Model, RelationshipPolicy, RelationshipCardinality } from "./Model";
 export { Record } from "./Record";
 export { List } from "./List";
 export { Mock } from "./Mock";
+export { FireModel } from "./FireModel";
+export { Audit } from "./Audit";
 
 export { fk, pk } from "common-types";
 export { key as fbKey } from "firebase-key";
