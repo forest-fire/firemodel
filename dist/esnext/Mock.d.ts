76c6b76cc48595852aaeff8271fee65fed838c6e
Ken Snyder <ken@ken.net>
2019-08-28 00:58:20 -0700

    Mock.generate() has stronger typing

--- dist/esnext/Mock.d.ts
+++ dist/esnext/Mock.d.ts
@@ -1,7 +1,13 @@
 import { Model } from "./Model";
 import { RealTimeDB } from "abstracted-firebase";
+/**
+ * Provides a _Model_ aware means of mocking your data.
+ *
+ * @param modelConstructor The Model being mocked
+ * @param db optionally state the DB connection; will use **Firemodel**'s default DB otherwise
+ */
 export declare function Mock<T extends Model>(modelConstructor: new () => T, db?: RealTimeDB): {
-    generate(count: number, exceptions?: import("common-types").IDictionary<any>): Promise<import("./Mock/types").IMockResponse<T>[]>;
+    generate(count: number, exceptions?: Partial<T>): Promise<import("./Mock/types").IMockResponse<T>[]>;
     createRelationshipLinks(cardinality?: import("common-types").IDictionary<number | true | [number, number]>): any;
     dynamicPathBehavior(options: string): any;
     overridesPassThrough(): any;
